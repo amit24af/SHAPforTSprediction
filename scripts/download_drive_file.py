de7dfe422b2daef3e148f802850e9a5864ffbc7d
amit24af <98124574+amit24af@users.noreply.github.com>
2025-08-18 03:18:13 +0200

    Update download_drive_file.py

--- scripts/download_drive_file.py
+++ scripts/download_drive_file.py
@@ -20,7 +20,7 @@ FOLDER_TO_DOWNLOAD = ('1gTo-lWF8KePNcNbhXSlaP6B6OusKnTeJ', 'data')
 # OUTPUT_FILE2 = 'descriptive_stats_experiment_data.ipynb'
 
 FILES_TO_DOWNLOAD = [
-    ('18EesyISDITHMX-VLI4mjvDF4nOQ7fDsz', 'experiment_FBProphet.ipynb'),
+    ('1LMBcxc6szfgdVrZi_3GTTvmexpUVZCL7', 'experiment_FBProphet.ipynb'),
     ('1wY7X3QV_0O1uS3D7s0qmanF8O2aNA331', 'experiment_XGBoost.ipynb'),
     ('1nLmPj6MKZecHK7ywYXKiWKtg4dr1jtUu', 'experiment_LSTM.ipynb'),
     ('1iuDYJ7wqm4Bh8iIPvPKylCBnsc0aLPN6', 'descriptive_stats_experiment_data.ipynb'),
